caff38d9b602dbc6547b003f76ae77f0da799f17
Meiling Thompson <meilinglthompson@gmail.com>
2019-07-29 23:43:48 -0400

    Progress on problem 3.1

---
+++ Chap3/src/Prob1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+// Meiling Thompson
+// CtCI Prob 3.1: Three in One: Describe how you could use a single array to implement three stacks.
+// Array divided into three equally long segments
+import java.util.*;
+
+public class Prob1 {
+	
+	public static void main( String[] args ) {
+		Scanner in = new Scanner(System.in);
+	}
+}
